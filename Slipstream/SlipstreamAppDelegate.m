eadd3650a5ddc3f5bd9ecf39a395fa0275bcffd5
David Carr <dcarr@scienceprousa.com>
2015-09-05 03:11:48 -0400

    add missing files

---
+++ Slipstream/SlipstreamAppDelegate.m
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#import <Parse/Parse.h>
+
+#import "SlipstreamAppDelegate.h"
+#import "SlipstreamViewController.h"
+#import "ZipArchive.h"
+
+@implementation SlipstreamAppDelegate
+
+#pragma mark -
+#pragma mark UIApplicationDelegate
+
+NSString *serviceURL = @"http://10.59.71.38:3000";
+NSString *channelName = @"ios-demo";
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [Parse enableLocalDatastore];
+    
+    [PFUser enableAutomaticUser];
+    
+    PFACL *defaultACL = [PFACL ACL];
+
+    [defaultACL setPublicReadAccess:YES];
+
+    [PFACL setDefaultACL:defaultACL withAccessForCurrentUser:YES];
+
+    self.window.rootViewController = self.viewController;
+    [self.window makeKeyAndVisible];
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000
+    if ([application respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+        UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert |
+                                                        UIUserNotificationTypeBadge |
+                                                        UIUserNotificationTypeSound);
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes
+                                                                                 categories:nil];
+        [application registerUserNotificationSettings:settings];
+        [application registerForRemoteNotifications];
+    } else
+#endif
+    {
+        [application registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge |
+                                                         UIRemoteNotificationTypeAlert |
+                                                         UIRemoteNotificationTypeSound)];
+    }
+
+    return YES;
+}
+
+#pragma mark Push Notifications
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    [currentInstallation setDeviceTokenFromData:deviceToken];
+    [currentInstallation saveInBackground];
+
+    [PFPush subscribeToChannelInBackground:channelName block:^(BOOL succeeded, NSError *error) {
+        if (succeeded) {
+            NSLog(@"Slipstream successfully subscribed to push notifications on the broadcast channel.");
+        } else {
+            NSLog(@"Slipstream failed to subscribe to push notifications on the broadcast channel.");
+        }
+    }];
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    if (error.code == 3010) {
+        NSLog(@"Push notifications are not supported in the iOS Simulator.");
+    } else {
+        // show some alert or otherwise handle the failure to register.
+        NSLog(@"application:didFailToRegisterForRemoteNotificationsWithError: %@", error);
+    }
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    if (application.applicationState == UIApplicationStateInactive) {
+        [PFAnalytics trackAppOpenedWithRemoteNotificationPayload:userInfo];
+    }
+    NSLog(@"didReceiveRemoteNotification");
+//    for(NSString *key in [userInfo allKeys]) {
+//        NSLog(@"%@",[userInfo objectForKey:key]);
+//    }
+    NSString *filepath = [[NSBundle mainBundle] pathForResource:@"ZipFileName" ofType:@"zip"];
+    ZipArchive *zipArchive = [[ZipArchive alloc] init];
+
+//    [zipArchive UnzipOpenFile:filepath Password:@"xxxxxx"];
+//    [zipArchive UnzipFileTo:{pathToDirectory} overWrite:YES];
+//    [zipArchive UnzipCloseFile];
+//    [zipArchive release];
+    completionHandler(UIBackgroundFetchResultNewData);
+}
+
+@end
